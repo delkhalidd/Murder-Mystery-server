ba24ea93c4e43ed0863ae6a6b39b49949145012e
riapatterson <riapatterson@hotmail.co.uk>
2025-07-28 16:17:17 +0100

    Added login/register functions to user controller with password encryption

---
+++ controller/UserController.js
@@ -0,0 +1,56 @@
+const bcrypt = require("bcrypt");
+const jwt = reuqire("jsonwebtoken");
+
+const User = require("../model/User");
+
+async function register(req, res) {
+    try {
+        const data = req.body;
+
+        const salt = await bcrypt.genSalt(parseInt(process.env.BCRYPT_SALT_ROUNDS));
+
+        data["password"] = await bcrypt.hash(data.password, salt);
+        //console.log(data);
+        const result = await User.create(data)
+
+        res.status(201).send(result);
+    } catch (err) {
+        res.status(400).json({ error: err.message })
+    }
+}
+
+async function login(req, res) {
+    const data = req.body;
+    try {
+        const user = await User.getOneByUsername(data.username);
+
+        if(!user) { 
+            throw new Error('No user with this username') 
+        }
+
+        const match = await bcrypt.compare(data.password, user.password);
+  
+        if (match) {
+
+            const payload = { username: user.username }
+
+            const sendToken = (err, token) => {
+                if (err) { 
+                    throw new Error('Error in token generation') 
+                }
+                res.status(200).json({
+                    success: true,
+                    token: token
+                });
+        }
+
+        jwt.sign(payload, process.env.SECRET_TOKEN, { expiresIn: 7200 }, sendToken);
+
+        } else {
+            throw new Error('User could not be authenticated')  
+        }
+        
+    } catch (err) {
+      res.status(401).json({ error: err.message });
+    }
+}
