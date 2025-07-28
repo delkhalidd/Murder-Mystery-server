544b9374af2083b86088b983c7e7dda153177180
solithcy <me@rose.gay>
2025-07-28 14:13:35 +0100

    fix missing commas + drop order

--- database/database.sql
+++ database/database.sql
@@ -1,32 +1,32 @@
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS cases;
-DROP TABLE IF EXISTS accepted_invites;
-DROP TABLE IF EXISTS briefs;
-DROP TABLE IF EXISTS teacher_input;
-DROP TABLE IF EXISTS questions;
 DROP TABLE IF EXISTS answers;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS teacher_input;
+DROP TABLE IF EXISTS briefs;
+DROP TABLE IF EXISTS accepted_invites;
+DROP TABLE IF EXISTS cases;
+DROP TABLE IF EXISTS users;
 
 
 CREATE TABLE users (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    firstname VARCHAR(30) NOT NULL,
-    surnames VARCHAR(30) NOT NULL,
-    username VARCHAR(30) UNIQUE NOT NULL,
-    email VARCHAR(50) UNIQUE NOT NULL,
-    password CHAR(60) NOT NULL,
-    account_type INT NOT NULL,
-    PRIMARY KEY (id)
+   id INT GENERATED ALWAYS AS IDENTITY,
+   firstname VARCHAR(30) NOT NULL,
+   surnames VARCHAR(30) NOT NULL,
+   username VARCHAR(30) UNIQUE NOT NULL,
+   email VARCHAR(50) UNIQUE NOT NULL,
+   password CHAR(60) NOT NULL,
+   account_type INT NOT NULL,
+   PRIMARY KEY (id)
 );
 
 CREATE TABLE cases (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    date_created DATE NOT NULL,
-    created_by INT NOT NULL
-    title VARCHAR(50) NOT NULL,
-    description TEXT,
-    invite_token VARCHAR(64) NOT NULL, 
-    PRIMARY KEY (id),
-    FOREIGN KEY (created_by) REFERENCES users(id) 
+   id INT GENERATED ALWAYS AS IDENTITY,
+   date_created DATE NOT NULL,
+   created_by INT NOT NULL,
+   title VARCHAR(50) NOT NULL,
+   description TEXT,
+   invite_token VARCHAR(64) NOT NULL,
+   PRIMARY KEY (id),
+   FOREIGN KEY (created_by) REFERENCES users(id)
 );
 
 CREATE TABLE accepted_invites (
@@ -77,7 +77,7 @@ CREATE TABLE answers (
     answer TEXT NOT NULL,
     date_created DATE NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (case_id) REFERENCES cases(id)
+    FOREIGN KEY (case_id) REFERENCES cases(id),
     FOREIGN KEY (question_id) REFERENCES questions(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
