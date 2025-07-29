1ed7b877f8f6f0a1220a58a050c68cd96fc5f65a
solithcy <me@rose.gay>
2025-07-29 15:22:47 +0100

    move entrypoint to index.js

--- app.js
+++ app.js
@@ -17,8 +17,4 @@ app.use((req, res) => {
   res.status(404).end();
 });
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, ()=>{
-  console.log(`Listening on port ${port}`);
-});
+module.exports = app;
