e641e09f426311f13738d3b920ba97c63700b651
riapatterson <riapatterson@hotmail.co.uk>
2025-07-29 12:48:00 +0100

    undone chanhes to path directiory to model/user

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("/model/User");
+const User = require("../model/User");
 
 const authMiddleware = (authRequired) => {
   return async (req, res, next) => {
