f0c94a88b6bd571354bd9409559fefc7691d3f82
solithcy <me@rose.gay>
2025-07-30 12:50:51 +0100

    dont throw error on no rows

--- model/Answer.js
+++ model/Answer.js
@@ -15,7 +15,7 @@ class Answer {
         const res = await db.query("INSERT INTO answers (case_id, question_id, user_id, correct, answer, created_at) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
             [case_id, question_id, user_id, correct, answer, new Date()]
         );
-    
+
         if(res.rows.length === 0) throw new Error("couldn't create answer");
 
         return new Answer(res.rows[0]);
@@ -29,25 +29,21 @@ class Answer {
 
     static async getByCase(cId) {
         const res = await db.query("SELECT * FROM answers WHERE case_id = $1", [cId]);
-        if(res.rows.length === 0) throw new Error("no answers found for this case");
         return res.rows.map(row => new Answer(row));
     }
 
     static async getByQuestion(qId) {
         const res = await db.query("SELECT * FROM answers WHERE question_id = $1", [qId]);
-        if(res.rows.length === 0) throw new Error("no answers found for this question");
         return res.rows.map(row => new Answer(row));
     }
 
     static async getByUser(uId) {
         const res = await db.query("SELECT * FROM answers WHERE user_id = $1", [uId]);
-        if(res.rows.length === 0) throw new Error("no answers found for this user");
         return res.rows.map(row => new Answer(row));
     }
 
     static async getByUserAndQuestion(uId, qId) {
         const res = await db.query("SELECT * FROM answers WHERE user_id = $1 AND question_id = $2", [uId, qId]);
-        if(res.rows.length === 0) throw new Error("no answers found for this question by this user");
         return res.rows.map(row => new Answer(row));
     }
 }
