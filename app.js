5323f647116250c91bb43ac3b71eaf6c64943b74
solithcy <me@rose.gay>
2025-07-30 23:23:39 +0100

    remove testing domain (oops!)

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser')
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:3001', 'http://localhost:3000', "https://poultry-logan-favorite-nt.trycloudflare.com"],
+  origin: ['http://localhost:3001', 'http://localhost:3000'],
   credentials: true,
 }));
 app.use(cookieParser());
